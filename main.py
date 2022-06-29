4261985b0069d1ad0e3a0ed422c2168724c54ff4
yuhui liang <liangyuhui@matrixtime.com>
2022-06-29 20:14:36 +0800

    revise generate_picture

--- main.py
+++ main.py
@@ -1,17 +1,46 @@
 from scipy.optimize import linear_sum_assignment
+from matplotlib import pyplot as plt
 import numpy as np
 import random
 import math
 
 class Solution:
-    def generate_dots(self, number: int):  #造出number张图片，[[x1, y1], [x2, y2]]表示
+    def generate_dots(self, rows: int, cols: int):  #造出number张图片，[[x1, y1], [x2, y2]]表示
         dots = []
-        for _ in range(number):
-            x1 = random.randint(0, 1500 - 2) # 900 * 1500范围内
-            y1 = random.randint(0, 900 - 2)
-            x2 = random.randint(x1 + 1, 1500)
-            y2 = random.randint(y1 + 1, 900)
-            dots.append([[x1, y1],[x2, y2]])
+        first_dot_classes = [[[0,0],[300,500]],[[50,50],[800,900]],[[100,100],[1500,2000]]]
+        ratio1_classes = [0.15, 0.2, 0.25, 0.3] #同行间
+        ratio2_classes = [0.1, 0.15, 0.2, 0.23] #异行间
+        ratio3_classes = [0.05, 0.1, 0.15, 0.2] #图片本身缩放率
+        records = []
+        for i in range(rows):
+            first_dot = random.choice(first_dot_classes)
+            if i == 0:
+                records.append(first_dot[:])
+            else:
+                ratio2 = random.choice(ratio2_classes)
+                last_dot = records[-1]
+                xa, xb, ya, yb = last_dot[0][0], last_dot[1][0], last_dot[0][1], last_dot[1][1]
+                w = first_dot[1][0] - first_dot[0][0]
+                h = first_dot[1][1] - first_dot[0][1]
+                x1 = xa + int((xb - xa)*ratio2)
+                y1 = yb + int((yb - ya)*ratio2)
+                x2 = x1 + int(w*(1 + random.choice([1, -1])*ratio2))
+                y2 = y1 + int(h*(1 + random.choice([1, -1])*ratio2))
+                first_dot = [[x1, y1], [x2, y2]]
+                records.append(first_dot[:])
+            for j in range(cols):
+                if j == 0:
+                    dots.append(first_dot)
+                    continue
+                ratio1 = random.choice(ratio1_classes)
+                ratio3 = random.choice(ratio3_classes)
+                last_dot = dots[-1]
+                xa, xb, ya, yb = last_dot[0][0], last_dot[1][0], last_dot[0][1], last_dot[1][1]
+                x1 = xb + int((xb - xa)*ratio1)
+                y1 = ya + int((yb - ya)*ratio1)
+                x2 = x1 + int((xb - xa)*(1 + random.choice([1, -1])*ratio3))
+                y2 = y1 + int((yb - ya)*(1 + random.choice([1, -1])*ratio3))
+                dots.append([[x1, y1], [x2, y2]])
         return dots
 
     def Hugmatch(self, dots: list): #匈牙利匹配：从几种W、H拆解情况中，分别计算出最优匹配，最后选择损失最小的匹配方式
@@ -89,14 +118,28 @@ class Solution:
                 if count == index:
                     return i, j
                 count += 1
-
+    def drawPicture(self, dots: list):
+        left_top, right_down = dots[0], dots[1]
+        right_top = [right_down[0], left_top[1]]
+        left_down = [left_top[0], right_down[1]]
+        plt.plot([left_top[0], right_top[0], right_down[0], left_down[0]], 
+                [left_top[1], right_top[1], right_down[1], left_down[1]],'ro')
+        plt.plot([left_top[0], right_top[0]], [left_top[1], right_top[1]],'r')
+        plt.plot([right_top[0], right_down[0]], [right_top[1], right_down[1]],'r')
+        plt.plot([right_down[0], left_down[0]], [right_down[1], left_down[1]],'r')
+        plt.plot([left_down[0], left_top[0]], [left_down[1], left_top[1]],'r')
+ 
 if __name__ == "__main__":
     solution = Solution()
-    dots = solution.generate_dots(224) #造点
+    dots = solution.generate_dots(3,4) #造点
     print(dots)
-    left_top, right_down = solution.boundingBox(dots)
-    print('左上角：', left_top,'右下角：', right_down)
-    print(solution.resolve(len(dots)))
-    result = solution.Hugmatch(dots)
-    print(result)
+    print(len(dots))
+    for dot in dots:
+        solution.drawPicture(dot)
+    plt.show()
+    # left_top, right_down = solution.boundingBox(dots)
+    # print('左上角：', left_top,'右下角：', right_down)
+    # print(solution.resolve(len(dots)))
+    # result = solution.Hugmatch(dots)
+    # print(result)
 
